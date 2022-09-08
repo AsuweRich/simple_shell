0d02451091c964de4e38f739ac8e7d5bb3f19638
horluwathobiloba <dmbjanetkehinde@gmail.com>
2022-09-07 18:41:05 -0700

    Main.c file commit

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * main - initialize the variables of the program
  * @argc: number of values received from the command line
@@ -7,13 +6,12 @@
  * @env: number of values received from the command line
  * Return: zero on succes.
  */
-
 int main(int argc, char *argv[], char *env[])
 {
 	data_of_program data_struct = {NULL}, *data = &data_struct;
 	char *prompt = "";
 
-	initialize_data(data, argc, argv, env);
+	inicialize_data(data, argc, argv, env);
 
 	signal(SIGINT, handle_ctrl_c);
 
@@ -32,7 +30,6 @@ int main(int argc, char *argv[], char *env[])
  * when the signal SIGINT (ctrl + c) is send to the program
  * @UNUSED: option of the prototype
  */
-
 void handle_ctrl_c(int opr UNUSED)
 {
 	_print("\n");
@@ -40,14 +37,13 @@ void handle_ctrl_c(int opr UNUSED)
 }
 
 /**
- * initialize_data - inicialize the struct with the info of the program
+ * inicialize_data - inicialize the struct with the info of the program
  * @data: pointer to the structure of data
  * @argv: array of arguments pased to the program execution
  * @env: environ pased to the program execution
  * @argc: number of values received from the command line
  */
-
-void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
+void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 {
 	int i = 0;
 
@@ -62,38 +58,37 @@ void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
 	{
 		data->file_descriptor = open(argv[1], O_RDONLY);
 		if (data->file_descriptor == -1)
-		{
+															{
 			_printe(data->program_name);
 			_printe(": 0: Can't open ");
 			_printe(argv[1]);
 			_printe("\n");
 			exit(127);
-												}
+		}
 	}
 	data->tokens = NULL;
 	data->env = malloc(sizeof(char *) * 50);
 	if (env)
 	{
 		for (; env[i]; i++)
-		{
+															{
 			data->env[i] = str_duplicate(env[i]);
 		}
-	}
+														}
 	data->env[i] = NULL;
 	env = data->env;
-	data->alias_list = malloc(sizeof(char *) * 20);
+
+														data->alias_list = malloc(sizeof(char *) * 20);
 	for (i = 0; i < 20; i++)
 	{
 		data->alias_list[i] = NULL;
 	}
 }
-
 /**
  * sisifo - its a infinite loop that shows the prompt
  * @prompt: prompt to be printed
  * @data: its a infinite loop that shows the prompt
  */
-
 void sisifo(char *prompt, data_of_program *data)
 {
 	int error_code = 0, string_len = 0;
@@ -102,6 +97,7 @@ void sisifo(char *prompt, data_of_program *data)
 	{
 		_print(prompt);
 		error_code = string_len = _getline(data);
+
 		if (error_code == EOF)
 		{
 			free_all_data(data);
